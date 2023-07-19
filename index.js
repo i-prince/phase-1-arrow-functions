45e4cbf1a6c58e4010ef979fc27352ddf29ea40b
i-prince <molatonny@gmail.com>
2023-07-19 12:41:52 +0300

    Complete Assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = x => 2000 / 100;
+console.log(divide());
+
+const square = (x) => {
+    return x * x;
+}
+console.log(square(2));
+
+const add = (a, b) => a + b;
+console.log(add(3, 4));
